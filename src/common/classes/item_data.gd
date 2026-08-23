5c8567e683cd9635dc6c6cf1fb463c94e1887e11
gabsvm <53277787+gabsvm@users.noreply.github.com>
2026-08-23 11:39:49 -0300

    feat: add contextual world-use animation metadata to items

--- src/common/classes/item_data.gd
+++ src/common/classes/item_data.gd
@@ -6,5 +6,6 @@ extends Resource
 @export var name: String = ""
 @export_multiline var description: String = ""
 @export var icon: Texture2D
+@export_enum("", "use_mid", "pickup_low", "inspect", "none") var world_use_animation: String = ""
 @export var combinable_with: Array[ItemData] = []
 @export var resulting_item: ItemData = null
